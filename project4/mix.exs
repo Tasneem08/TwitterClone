74cb12f57f1093110ff98f4930dbe5f41d0e5814
Tasneem08 <tasneem08@ufl.edu>
2017-11-18 16:06:57 -0500

    Adding escript in mix file

--- project4/mix.exs
+++ project4/mix.exs
@@ -7,7 +7,8 @@ defmodule Project4.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: [main_module: MainController]
     ]
   end
 
